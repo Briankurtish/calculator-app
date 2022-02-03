a7a99692027a6002ecdae8eda40fbffa8b0fcc43
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-02-03 09:05:37 +0100

    app completed

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -30,6 +30,13 @@ class BmiCalculator extends StatefulWidget {
 class _BmiCalculatorState extends State<BmiCalculator> {
   // declaring a variable for the custom radio button
   int currentindex = 0;
+  String result = "";
+  double height = 0.0;
+  double weight = 0.0;
+
+  //Declaring the input controllers
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +54,11 @@ class _BmiCalculatorState extends State<BmiCalculator> {
           backgroundColor: Color(0xfffafafa),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                double height = double.parse(heightController.value.text);
+                double weight = double.parse(weightController.value.text);
+                calculateBmi(height, weight);
+              },
               icon: const Icon(
                 Icons.settings,
                 color: Colors.black,
@@ -69,8 +80,110 @@ class _BmiCalculatorState extends State<BmiCalculator> {
                     radioButton("Woman", Colors.pink, 1),
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20.0,
+                ),
+                //creating input form
+                const Text(
+                  "Your Height in Cm :",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  // adding the controllers to the input field
+                  controller: heightController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Your height in Cm",
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  "Your Weight in Kg :",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: weightController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Your Weight in Kg",
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50.0,
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        height = double.parse(heightController.value.text);
+                        weight = double.parse(weightController.value.text);
+                      });
+                      calculateBmi(height, weight);
+                    },
+                    color: Colors.blue,
+                    child: const Text(
+                      "Calculate",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: const Text(
+                    "Your BMI is :",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -78,6 +191,14 @@ class _BmiCalculatorState extends State<BmiCalculator> {
         ),
       ),
     );
+  }
+
+  void calculateBmi(double height, double weight) {
+    double finalresult = weight / (height * height / 10000);
+    String bmi = finalresult.toStringAsFixed(2);
+    setState(() {
+      result = bmi;
+    });
   }
 
   // declaring a function to change the index value on button pressed
