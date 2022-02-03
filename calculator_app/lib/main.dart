267b545ab14931bb8b9af67cc9c1d4148c8a826a
Cipher <67450280+Briankurtish@users.noreply.github.com>
2022-02-03 03:16:23 +0100

    added radio button and functions

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -28,6 +28,9 @@ class BmiCalculator extends StatefulWidget {
 }
 
 class _BmiCalculatorState extends State<BmiCalculator> {
+  // declaring a variable for the custom radio button
+  int currentindex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -53,6 +56,61 @@ class _BmiCalculatorState extends State<BmiCalculator> {
           ],
         ),
         // Creating thr body of the app
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    radioButton("Man", Colors.blue, 0),
+                    radioButton("Woman", Colors.pink, 1),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // declaring a function to change the index value on button pressed
+
+  void changeIndex(int index) {
+    setState(() {
+      currentindex = index;
+    });
+  }
+
+  // creating a new custom widget
+  Widget radioButton(String value, Color color, int index) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 12.0),
+        height: 80.0,
+        child: FlatButton(
+          color: currentindex == index ? color : Colors.grey[200],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          onPressed: () {
+            changeIndex(index);
+          },
+          child: Text(
+            value,
+            style: TextStyle(
+              color: currentindex == index ? Colors.white : color,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
